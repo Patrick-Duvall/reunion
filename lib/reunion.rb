7b66008e63885314f9dd6120cc9c6b784a720627
Patrick Duvall <duvallp208@gmail.com>
2019-03-14 18:40:57 -0600

    Complete iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Reunion
   attr_reader :name, :activities
   def initialize(name)
@@ -7,6 +9,37 @@ class Reunion
 
   def add_activity(activity)
     @activities << activity
+  end
+
+  def total_cost
+    @activities.map{|activity|activity.total_cost}.sum
+  end
+
+  def participants
+    @activities.map{|activity|activity.participants.keys}.flatten
+  end
+
+  def owed_per_activity
+    @activities.map(&:total_owed)
+  end
+
+
+  def breakout
+    pay_sheet = owed_per_activity.reduce{ |activity, next_activity| activity.merge(next_activity){|person, paid_1,paid_2| paid_1 + paid_2 }}
+    pay_sheet
+  end
+
+  def summary
+
+    breakout.map{|person, owes| "#{person}: #{owes}\n"}.join.chomp
+
+  end
+
+  def detailed_breakout
+    breakout = {}
+    participants.each{|participant| breakout[participant] = []}
+    binding.pry
+
   end
 
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,7 +4,6 @@ require "minitest/autorun"
 
 class ReunionTest < Minitest::Test
 
-
 def setup
   @reunion = Reunion.new('1406 BE')
   @activity_1 = Activity.new("Brunch")
@@ -21,5 +20,140 @@ def test_has_and_can_add_activities
   assert_equal [@activity_1], @reunion.activities
 end
 
+def test_owed_per_activity
+  @activity_1.add_participant("Maria", 20)
+  @activity_1.add_participant("Luther", 40)
+  @reunion.add_activity(@activity_1)
+  assert_equal 60, @reunion.total_cost
+  @activity_2.add_participant("Maria", 60)
+  @activity_2.add_participant("Luther", 60)
+  @activity_2.add_participant("Louis", 0)
+  @reunion.add_activity(@activity_2)
+  assert_equal [{"Maria"=>10, "Luther"=>-10}, {"Maria"=>-20, "Luther"=>-20, "Louis"=>40}], @reunion.owed_per_activity
+end
 
+def test_participants
+  @activity_1.add_participant("Maria", 20)
+  @activity_1.add_participant("Luther", 40)
+  @activity_2.add_participant("Louis", 0)
+  @reunion.add_activity(@activity_1)
+  @reunion.add_activity(@activity_2)
+  assert_equal %w(Maria Luther Louis), @reunion.participants
+end
+
+def test_breakout
+
+  @activity_1.add_participant("Maria", 20)
+  @activity_1.add_participant("Luther", 40)
+  @reunion.add_activity(@activity_1)
+  assert_equal 60, @reunion.total_cost
+  @activity_2.add_participant("Maria", 60)
+  @activity_2.add_participant("Luther", 60)
+  @activity_2.add_participant("Louis", 0)
+  @reunion.add_activity(@activity_2)
+  assert_equal 180, @reunion.total_cost
+  assert_equal ( {"Maria" => -10, "Luther" => -30, "Louis" => 40}), @reunion.breakout
+  assert_equal "Maria: -10\nLuther: -30\nLouis: 40", @reunion.summary
+end
+
+def test_detailed_breakout
+  reunion = Reunion.new("1406 BE")
+
+  activity_1 = Activity.new("Brunch")
+  activity_1.add_participant("Maria", 20)
+  activity_1.add_participant("Luther", 40)
+
+  activity_2 = Activity.new("Drinks")
+  activity_2.add_participant("Maria", 60)
+  activity_2.add_participant("Luther", 60)
+  activity_2.add_participant("Louis", 0)
+
+  activity_3 = Activity.new("Bowling")
+  activity_3.add_participant("Maria", 0)
+  activity_3.add_participant("Luther", 0)
+  activity_3.add_participant("Louis", 30)
+
+  activity_4 = Activity.new("Jet Skiing")
+  activity_4.add_participant("Maria", 0)
+  activity_4.add_participant("Luther", 0)
+  activity_4.add_participant("Louis", 40)
+  activity_4.add_participant("Nemo", 40)
+
+  reunion.add_activity(activity_1)
+  reunion.add_activity(activity_2)
+  reunion.add_activity(activity_3)
+  reunion.add_activity(activity_4)
+
+  assert_equal ({
+    "Maria" => [
+      {
+        activity: "Brunch",
+        payees: ["Luther"],
+        amount: 10
+      },
+      {
+        activity: "Drinks",
+        payees: ["Louis"],
+        amount: -20
+      },
+      {
+        activity: "Bowling",
+        payees: ["Louis"],
+        amount: 10
+      },
+      {
+        activity: "Jet Skiing",
+        payees: ["Louis", "Nemo"],
+        amount: 10
+      }
+    ],
+    "Luther" => [
+      {
+        activity: "Brunch",
+        payees: ["Maria"],
+        amount: -10
+      },
+      {
+        activity: "Drinks",
+        payees: ["Louis"],
+        amount: -20
+      },
+      {
+        activity: "Bowling",
+        payees: ["Louis"],
+        amount: 10
+      },
+      {
+        activity: "Jet Skiing",
+        payees: ["Louis", "Nemo"],
+        amount: 10
+      }
+    ],
+    "Louis" => [
+      {
+        activity: "Drinks",
+        payees: ["Maria", "Luther"],
+        amount: 20
+      },
+      {
+        activity: "Bowling",
+        payees: ["Maria", "Luther"],
+        amount: -10
+      },
+      {
+        activity: "Jet Skiing",
+        payees: ["Maria", "Luther"],
+        amount: -10
+      }
+    ],
+    "Nemo" => [
+      {
+        activity: "Jet Skiing",
+        payees: ["Maria", "Luther"],
+        amount: -10
+      }
+    ]
+  }), reunion.detailed_breakout
+
+end
 end
